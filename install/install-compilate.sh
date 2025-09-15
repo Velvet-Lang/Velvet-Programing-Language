c9ce4a31b68c248ab62b729c77d0b9ca9ae287b6
Michal92299 <michal.99967@gmail.com>
2025-09-15 14:20:55 +0200

    Update install-compilate.sh

--- install/install-compilate.sh
+++ install/install-compilate.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+echo "[CHECKING AND COMPILING...]"
+echo "[RUN] Cloning git repo."
+git clone https://github.com/Velvet-Lang/Velvet-Programing-Language.git /tmp/
+cd /tmp/Velvet-Programing-Language/
+echo "[INFO] INSTALL TOOLS"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+2
 
+stable
+echo "[BUILD] Building Binars..."
+echo "[MOVE] Transfering Binaras.."
+sudo mv /tmp/Velvet-Programing-Language/target/release/weave /usr/bin/
+sudo 
