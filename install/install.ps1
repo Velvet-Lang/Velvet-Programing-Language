7261039c5a0ec88b8aff834d5730101b5a1c7f45
VoidArc Studio <chudybyczek2137@gmail.com>
2025-09-15 19:06:05 +0200

    Update install.ps1

--- install/install.ps1
+++ install/install.ps1
@@ -1,1 +1,15 @@
-
+Write-Host "[INFO] Downloading binary file..."
+$TempPath = "$env:TEMP\weave"
+curl -L -o $TempPath https://github.com/Velvet-Lang/Velvet-Programing-Language/releases/download/v0.1/weave
+$InstallPath = "$env:ProgramFiles\Weave"
+if (-not (Test-Path $InstallPath)) {
+    New-Item -ItemType Directory -Path $InstallPath | Out-Null
+}
+Move-Item $TempPath "$InstallPath\weave.exe" -Force
+Write-Host "[DONE] Now run weave help"
+for ($i = 10; $i -ge 1; $i--) {
+    Write-Host "$i seconds to clear" -NoNewline
+    Start-Sleep -Seconds 1
+    Write-Host "`r" -NoNewline
+}
+Clear-Host
