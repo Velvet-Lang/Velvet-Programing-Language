149521bce9276e29a8110de93ef75e8953d773e4
VoidArc Studio <chudybyczek2137@gmail.com>
2025-09-15 20:04:36 +0200

    Delete build.rs

--- build.rs
+++
@@ -1,7 +0,0 @@
-// core/build.rs
-fn main() {
-    cc::Build::new()
-        .file("weave/src/main.c")  // Link do weave C
-        .compile("weave");
-    println!("cargo:rerun-if-changed=weave/src/");
-}
