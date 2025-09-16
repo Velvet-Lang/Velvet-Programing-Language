a1cd78c91c749b0c08c4668d38a4c4ec1f334b25
Michal92299 <michal.99967@gmail.com>
2025-09-16 15:14:15 +0200

    Update main.py

--- vel/main.py
+++ vel/main.py
@@ -1,23 +1,32 @@
 import sys
 from rich.console import Console
 from .interpreter import interpret_o, interpret_velvet
+from .runtime import load_o, load_velvet, hot_reload
 
 console = Console(style="bold magenta")  # Cyberpunk purple
 
 def main():
     if len(sys.argv) < 2:
-        console.print("[red]Usage: vel <file.o|file.velvet>[/]")
+        console.print("[red]Usage: vel <file.o|file.velvet> [--hot-reload][/]")
         sys.exit(1)
 
     file = sys.argv[1]
+    hot = "--hot-reload" in sys.argv
+
     if file.endswith(".o"):
         console.print(f"[cyan]Executing {file}...[/]")
-        interpret_o(file)
+        if hot:
+            hot_reload(file, interpret_o)
+        else:
+            interpret_o(file)
     elif file.endswith(".velvet"):
         console.print(f"[cyan]Executing {file}...[/]")
-        interpret_velvet(file)
+        if hot:
+            hot_reload(file, interpret_velvet)
+        else:
+            interpret_velvet(file)
     else:
-        console.print("[red]Invalid file type: Must be .o or .velvet[/]")
+        console.print("[red]Invalid file type[/]")
         sys.exit(1)
 
 if __name__ == "__main__":
