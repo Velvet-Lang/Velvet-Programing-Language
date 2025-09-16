1cbf83154b500a7db6097c77a79f36251f248feb
Michal92299 <michal.99967@gmail.com>
2025-09-16 13:54:37 +0200

    Update and rename weave/run.py to weave/src/deps.rs

---
+++ weave/src/deps.rs
@@ -0,0 +1,44 @@
+use std::fs;
+use std::io::Write;
+use crossterm::{
+    style::{Color, Print, ResetColor, SetForegroundColor},
+    ExecutableCommand,
+};
+use std::io::stdout;
+
+pub fn install_dep(lib: &str) {
+    stdout()
+        .execute(SetForegroundColor(Color::Cyan))
+        .unwrap()
+        .execute(Print(format!("Installing dependency {}...\n", lib)))
+        .unwrap()
+        .execute(ResetColor)
+        .unwrap();
+    // Placeholder: Download from Velvet repo or system package manager
+}
+
+pub fn install_in(lib: &str) {
+    let mut file = fs::OpenOptions::new()
+        .append(true)
+        .open("weave.velvet")
+        .unwrap();
+    file.write_all(format!("{}\n", lib).as_bytes()).unwrap();
+    stdout()
+        .execute(SetForegroundColor(Color::Green))
+        .unwrap()
+        .execute(Print(format!("Added {} to weave.velvet\n", lib)))
+        .unwrap()
+        .execute(ResetColor)
+        .unwrap();
+}
+
+pub fn install_o(lib: &str) {
+    fs::create_dir_all("isolated_env").unwrap();
+    stdout()
+        .execute(SetForegroundColor(Color::Green))
+        .unwrap()
+        .execute(Print(format!("Installed {} in isolated_env/\n", lib)))
+        .unwrap()
+        .execute(ResetColor)
+        .unwrap();
+}
