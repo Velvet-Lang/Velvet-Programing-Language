ae7e20991467d941d0fc3978d7e9d2b6c3663b87
Michal92299 <michal.99967@gmail.com>
2025-09-16 13:58:47 +0200

    Update optimizer.rs

--- core/src/optimizer.rs
+++ core/src/optimizer.rs
@@ -1,7 +1,28 @@
 use crate::parser::AstNode;
 
 pub fn optimize_ast(ast: Vec<AstNode>) -> Vec<AstNode> {
-    // Dead code elim: Remove comments
-    ast.into_iter().filter(|node| !matches!(node, AstNode::Comment(_))).collect()
-    // Inlining: Stub for now
+    let mut optimized = Vec::new();
+    for node in ast {
+        match node {
+            AstNode::Comment(_) => {} // Dead code elimination
+            AstNode::Loop(count, cmd) => {
+                if let Ok(n) = count.parse::<u32>() {
+                    if n <= 3 {
+                        // Unroll small loops
+                        for i in 0..n {
+                            let mut new_cmd = cmd.clone();
+                            if let AstNode::Output(ref mut s) = new_cmd.as_mut() {
+                                *s = s.replace("$INDEX", &i.to_string());
+                            }
+                            optimized.push(*new_cmd);
+                        }
+                        continue;
+                    }
+                }
+                optimized.push(AstNode::Loop(count, cmd));
+            }
+            _ => optimized.push(node),
+        }
+    }
+    optimized
 }
